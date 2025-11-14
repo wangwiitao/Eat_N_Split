43a5e79aa5854d3f8da8a6397e03d3f616237ab4
975569220@qq.com <975569220@qq.com>
2025-11-14 23:06:13 +0800

    add a form

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -57,7 +60,45 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even.</p>}
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👫 Friend Name</label>
+      <input type="text" />
+      <label>🌄 Image URL</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Form Split Bill</h2>
+      <label>🌄Bill value </label>
+      <input type="text" />
+
+      <label>👫Your expense</label>
+      <input type="text" />
+
+      <label>🌄 X's expense</label>
+      <input type="text" disabled />
+
+      <label>💰 Who is paying the bill</label>
+      <select>
+        <option value="you">You</option>
+        <option value="x">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
   );
 }
